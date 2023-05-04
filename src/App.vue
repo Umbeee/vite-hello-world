d022f0dd1abaf5486b49852efbc70ee0f54908aa
Umbeee <umberto.nesta@hotmail.com>
2023-05-04 09:54:34 +0200

    finalmente sono riuscito a fare commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import helloWorld from './components/helloWorld.vue';
+
+  export default {
+    name: "App",
+    components: {
+      helloWorld
+    }
+  }
+</script>
+
+<template>
+<div>
+  <helloWorld/>
+</div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/helloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: "helloWorld"
+    }
+</script>
+
+<style>
+
+</style>
